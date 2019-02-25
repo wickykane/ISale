e12d067b86114907eb1e7f7e6c5a5eadf19758e5
wickykane <wickykane@gmail.com>
2019-02-25 18:55:55 +0700

    update

--- src/containers/Items/items.create.js
+++ src/containers/Items/items.create.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { withState, withHandlers, compose } from "recompose";
 import {
   Form,
   Button,
@@ -16,75 +17,98 @@ import {
   Collapse
 } from "antd";
 
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { EditorState } from "draft-js";
+
 const FormItem = Form.Item;
 const TabPane = Tabs.TabPane;
 const Option = Select.Option;
 
-class CreateDrawer extends React.PureComponent {
-  /**
-   * INIT STATE DATA
-   */
+const DrawerContainer = compose(
+  withState("attributes", "setAttributes", []),
+  withState("currentTab", "setCurrentTab", "1"),
+  withState("editor", "setEditor", EditorState.createEmpty()),
+  withState("fileList", "setFileList", []),
+  withState("reviewList", "setReviewList", []),
+  withState("unitList", "setUnitList", [])
+);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      fileList: [],
-      reviewList: [],
-      attributes: []
-    };
-  }
+const CreateDrawer = props => {
+  const {
+    attributes,
+    setAttributes,
+    fileList,
+    setFileList,
+    reviewList,
+    setReviewList,
+    unitList,
+    setUnitList,
+    editor,
+    setEditor,
+    currentTab,
+    setCurrentTab,
+    form,
+    page
+  } = props;
 
   /**
    * DRAWER HANDLING
    */
 
-  closeDrawer = () => {
-    this.props.form.resetFields();
-    this.props.actionPageData("current", null);
+  const closeDrawer = () => {
+    form.resetFields();
+    setAttributes([]);
+    setFileList([]);
+    setReviewList([]);
+    setUnitList([]);
+    setCurrentTab("1");
+    setEditor(EditorState.createEmpty());
+    props.actionPageData("current", null);
   };
 
   /**
    * FORM DATA AND VALIDATION
    */
 
-  onSubmit = e => {
+  const onSubmit = e => {
     e.preventDefault();
-    this.props.form.validateFields((err, value) => {
+    form.validateFields((err, value) => {
       if (!err) {
         console.log(value);
       }
     });
   };
 
-  hasErrors(fieldsError) {
-    return Object.keys(fieldsError).some(field => fieldsError[field]);
+  function hasErrors(fieldsError) {
+    return Object.keys(fieldsError).some(field => {
+      if(Array.isArray(field)) {
+        return field.some( item => field[item]);
+      }
+      return fieldsError[field];
+    });
   }
 
   /**
    * UPLOAD IMAGE HANDLING
    */
 
-  getBase64(img, callback) {
+  function getBase64(img, callback) {
     const reader = new FileReader();
     reader.addEventListener("load", () => callback(reader.result));
     reader.readAsDataURL(img);
   }
 
-  handleRemove = e => {
-    const { fileList, reviewList } = this.state;
+  const handleRemove = e => {
     const newFileList = [...fileList].filter(item => item.uid != e.uid);
     const newReviewList = [...reviewList].filter(item => item.uid != e.uid);
-    this.setState({
-      ...this.state,
-      fileList: newFileList,
-      reviewList: newReviewList
-    });
+    setFileList(newFileList);
+    setReviewList(newReviewList);
   };
 
-  handleChange = e => {
+  const handleChange = e => {
     if (!e.file.status) {
-      this.getBase64(e.file, url => {
-        const { fileList, reviewList } = this.state;
+      getBase64(e.file, url => {
         const newFileList = [...fileList, e.file];
         const newReviewList = [
           ...reviewList,
@@ -93,11 +117,8 @@ class CreateDrawer extends React.PureComponent {
             url
           }
         ];
-        this.setState({
-          ...this.state,
-          fileList: newFileList,
-          reviewList: newReviewList
-        });
+        setFileList(newFileList);
+        setReviewList(newReviewList);
       });
     }
   };
@@ -106,39 +127,58 @@ class CreateDrawer extends React.PureComponent {
    * ATTRIBUE HANDLING
    */
 
-  addAttribute = () => {
-    let attributes = [...this.state.attributes];
-    attributes.push({});
-    this.setState({
-      ...this.state,
-      attributes
-    });
+  const addAttribute = () => {
+    const newAttribute = [...attributes];
+    newAttribute.push({ id: attributes.length });
+    setAttributes(newAttribute);
   };
 
-  removeAtribute = index => {
-    console.log(this.props.form.getFieldsValue());
+  const removeAtribute = id => {
+    const newAttribute = [...attributes];
+    const index = newAttribute.findIndex(item => item.id == id);
+    newAttribute[index].deleted = true;
+    setAttributes(newAttribute);
+  };
 
-    let attributes = [...this.props.form.getFieldsValue().attributes];
-    attributes.splice(index, 1);
+  /**
+   * UNIT HANDLING
+   */
 
-    this.setState({
-      ...this.state,
-      attributes
-    });
+  const addUnit = () => {
+    const newList = [...unitList];
+    newList.push({ id: newList.length });
+    setUnitList(newList);
+  };
+
+  /**
+   * EDITOR HANDLING
+   */
+
+  const changeEditor = editor => {
+    setEditor(editor);
+  };
+
+  const removeUnit = id => {
+    const newList = [...unitList];
+    const index = newList.findIndex(item => item.id == id);
+    newList[index].deleted = true;
+    setUnitList(newList);
   };
 
   /**
    * RENDER HTML HANDLING
    */
 
-  render() {
-    const { getFieldDecorator, getFieldsError } = this.props.form;
-    const { reviewList } = this.state;
+  function render() {
+    const { getFieldDecorator, getFieldsError } = form;
 
     const formItemLayout = {
       labelCol: { span: 8 },
       wrapperCol: { span: 16 }
     };
+
+    const validAttribute = attributes.filter(i => !i.deleted);
+    const validUnit = unitList.filter(i => !i.deleted);
 
     const uploadButton = (
       <div>
@@ -148,7 +188,7 @@ class CreateDrawer extends React.PureComponent {
     );
 
     const informationTab = (
-      <Form layout="horizontal" onSubmit={this.onSubmit}>
+      <div>
         <Row>
           <Col md={8}>
             <Row>
@@ -264,9 +304,9 @@ class CreateDrawer extends React.PureComponent {
                 beforeUpload={() => false}
                 listType="picture-card"
                 fileList={reviewList}
-                onChange={this.handleChange}
+                onChange={handleChange}
                 showUploadList={{ showPreviewIcon: false }}
-                onRemove={this.handleRemove}
+                onRemove={handleRemove}
               >
                 {uploadButton}
               </Upload>
@@ -275,7 +315,7 @@ class CreateDrawer extends React.PureComponent {
           <Col md={24}>
             <Collapse defaultActiveKey={["1"]}>
               <Collapse.Panel header="Theo dõi thuộc tính" key="1">
-                {this.state.attributes.length > 0 ? (
+                {validAttribute.length > 0 ? (
                   <table className="table-value">
                     <thead>
                       <tr>
@@ -285,8 +325,8 @@ class CreateDrawer extends React.PureComponent {
                       </tr>
                     </thead>
                     <tbody>
-                      { this.state.attributes.map((item, index) => (
-                        <tr key={index}>
+                      {validAttribute.map((item, index) => (
+                        <tr key={item.id}>
                           <td>
                             <Select defaultValue="lucy" style={{ width: 200 }}>
                               <Select.OptGroup label="Chọn thuộc tính ...">
@@ -300,15 +340,15 @@ class CreateDrawer extends React.PureComponent {
                           </td>
                           <td>
                             <FormItem>
-                              {getFieldDecorator("attributes["+ index + "].value", {
-                              })(
-                                <Input placeholder="Nhập giá trị và enter" />
-                              )}
+                              {getFieldDecorator(
+                                "attributes[" + item.id + "].value",
+                                {}
+                              )(<Input placeholder="Nhập giá trị và enter" />)}
                             </FormItem>
                           </td>
                           <td>
                             <a
-                              onClick={() => this.removeAtribute(index)}
+                              onClick={() => removeAtribute(item.id)}
                               href="javascrip:void(0)"
                             >
                               <Icon type="close" />
@@ -321,49 +361,176 @@ class CreateDrawer extends React.PureComponent {
                 ) : (
                   ""
                 )}
-                <Button onClick={this.addAttribute} icon="plus">
+                <Button onClick={addAttribute} icon="plus">
                   Thêm thuộc tính
                 </Button>
               </Collapse.Panel>
             </Collapse>
           </Col>
+          <Col md={24}>
+            <Collapse className="mt-3" defaultActiveKey={["1"]}>
+              <Collapse.Panel header="Đơn vị tính" key="1">
+                <FormItem label="Đơn vị cơ bản">
+                  {getFieldDecorator("uom")(<Input className="col-md-2" />)}
+                </FormItem>
+                {validUnit.length > 0 ? (
+                  <table className="table-value">
+                    <thead>
+                      <tr>
+                        <th>Tên đơn vị</th>
+                        <th>Giá trị quy đổi</th>
+                        <th>Giá bán</th>
+                        <th>Mã hàng hóa</th>
+                        <th />
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {validUnit.map((item, index) => (
+                        <tr key={item.id}>
+                          <td>
+                            <FormItem>
+                              {getFieldDecorator(
+                                "units[" + item.id + "].name",
+                                {}
+                              )(<Input placeholder="" />)}
+                            </FormItem>
+                          </td>
+                          <td>
+                            <FormItem>
+                              {getFieldDecorator(
+                                "attributes[" + item.id + "].value",
+                                {
+                                  initialValue: 1
+                                }
+                              )(<InputNumber />)}
+                            </FormItem>
+                          </td>
+                          <td>
+                            <FormItem>
+                              {getFieldDecorator(
+                                "attributes[" + item.id + "].price",
+                                {
+                                  initialValue: 0
+                                }
+                              )(<InputNumber />)}
+                            </FormItem>
+                          </td>
+                          <td>
+                            <FormItem>
+                              {getFieldDecorator(
+                                "attributes[" + item.id + "].code"
+                              )(<Input placeholder="Mã hàng tự động" />)}
+                            </FormItem>
+                          </td>
+                          <td>
+                            <a
+                              onClick={() => removeUnit(item.id)}
+                              href="javascrip:void(0)"
+                            >
+                              <Icon type="close" />
+                            </a>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                ) : (
+                  ""
+                )}
+                <Button className="mt-2" onClick={addUnit} icon="plus">
+                  Thêm đơn vị tính
+                </Button>
+              </Collapse.Panel>
+            </Collapse>
+          </Col>
         </Row>
-        <FormItem className="text-center">
-          <Button
-            disabled={this.hasErrors(getFieldsError())}
-            type="primary"
-            htmlType="submit"
-          >
-            Save
-          </Button>
-          <Button className="ml-2" onClick={this.closeDrawer}>
-            Cancel
-          </Button>
-        </FormItem>
-      </Form>
+      </div>
+    );
+
+    const detailTab = (
+      <div>
+        <Row>
+          <div className="card">
+            <div className="card-header">Định mức tồn</div>
+            <div className="pt-3">
+              <Col md={12} className="pl-3 pr-3">
+                <FormItem {...formItemLayout} label="Ít nhất">
+                  {getFieldDecorator("min_qty")(<Input placeholder="" />)}
+                </FormItem>
+              </Col>
+              <Col md={12} className="pl-3 pr-3">
+                <FormItem {...formItemLayout} label="Nhiều nhất">
+                  {getFieldDecorator("max_qty")(<Input placeholder="" />)}
+                </FormItem>
+              </Col>
+            </div>
+          </div>
+        </Row>
+        <Row>
+          <div className="card mt-3">
+            <div className="card-header">Mô tả</div>
+            <div className="pt-3">
+              <Col md={24} className="pl-3 pr-3">
+                <Editor
+                  editorState={editor}
+                  editorClassName="item-editor"
+                  onEditorStateChange={changeEditor}
+                />
+              </Col>
+            </div>
+          </div>
+        </Row>
+      </div>
     );
 
     /**
      * Main render
      */
 
+    const page_Data = {
+      title: {
+        items: "Thêm hàng hóa",
+        service: "Thêm dịch vụ",
+        combo: "Thêm Combo"
+      }
+    };
+
     return (
       <Drawer
         width={"100%"}
-        onClose={this.closeDrawer}
-        visible={this.props.page.current == "items"}
-        title="Thêm hàng hóa"
+        destroyOnClose={true}
+        onClose={closeDrawer}
+        visible={["items", "service", "combo"].indexOf(page.current) != -1}
+        title={page_Data.title[page.current]}
       >
-        <Tabs defaultActiveKey="1">
-          <TabPane tab="Thông tin" key="1">
-            {informationTab}
-          </TabPane>
-          <TabPane tab="Mô tả chi tiết" key="2" />
-          <TabPane tab="Thành phần" key="3" />
-        </Tabs>
+        <Form layout="horizontal" onSubmit={onSubmit}>
+          <Tabs onChange={tab => setCurrentTab(tab)} activeKey={currentTab}>
+            <TabPane tab="Thông tin" key="1">
+              {informationTab}
+            </TabPane>
+            <TabPane tab="Mô tả chi tiết" key="2">
+              {detailTab}
+            </TabPane>
+            <TabPane tab="Thành phần" key="3" />
+          </Tabs>
+          <FormItem className="text-center mt-3">
+            <Button
+              disabled={hasErrors(getFieldsError())}
+              type="primary"
+              htmlType="submit"
+            >
+              Save
+            </Button>
+            <Button className="ml-2" onClick={closeDrawer}>
+              Cancel
+            </Button>
+          </FormItem>
+        </Form>
       </Drawer>
     );
   }
-}
-const ItemsCreateComponent = Form.create()(CreateDrawer);
+
+  return render();
+};
+const ItemsCreateComponent = DrawerContainer(Form.create()(CreateDrawer));
 export default ItemsCreateComponent;
